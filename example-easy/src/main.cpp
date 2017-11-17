09540258c43919478950bc7a47812b2bdc1da85f
Christopher Baker <me@christopherbaker.net>
2017-11-17 11:21:05 -0600

    Updates for 0.10.0

--- example-easy/src/main.cpp
+++ example-easy/src/main.cpp
@@ -1,9 +1,6 @@
-#include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 int main() {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024 + 32, 128 + 32, OF_WINDOW);
+	ofSetupOpenGL(1024 + 32, 128 + 32, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
--- example-process-input/src/main.cpp
+++ example-process-input/src/main.cpp
@@ -1,9 +1,6 @@
-#include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 int main() {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024, 800, OF_WINDOW);
+	ofSetupOpenGL(1024, 800, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
